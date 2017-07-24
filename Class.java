5c5e59838279fd95dbbd475f8190a779ea126b0f
jonathan-314 <jonguo6@gmail.com>
2017-07-24 13:02:45 -0700

    Add Student

--- Class.java
+++ Class.java
@@ -3,13 +3,20 @@ public class Class extends Course {
 	School school;
 	Teacher teacher;
 	String room;
+	int period;
 	int num;
 	Student[] students;
 	boolean percentage;
 	ArrayList<LearningTarget> targets;
-	public Class() {
-		num = 40;
+	int index = 0;
+	public Class(int number) {
+		num = number;
 		students = new Student[num];
 		targets = new ArrayList<LearningTarget>();
+	}
+	public void addStudent(Student st) {
+		students[index] = st;
+		st.index[period-1] = index;
+		index++;
 	}
 }
