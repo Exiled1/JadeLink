42a3af59efcfaf3d729a243ee7795c21bf471533
jonathan-314 <jonguo6@gmail.com>
2017-07-24 11:52:26 -0700

    test

--- Student.java
+++ Student.java
@@ -6,9 +6,30 @@ public class Student {
 	Class[] schedule;
 	int[] index;
 	School school;
+	double[] grades;
+	String[] letters;
 	public Student() {
 		schedule = new Class[8];
 		index = new int[8];
+		grades = new double[8];
+		letters = new String[8];
+	}
+	public void update() {
+		for(int i=0;i<8;i++) {
+			if(schedule[i].percentage) {
+				if(grades[i] >= 90) letters[i] = "A";
+				else if(grades[i] >= 80) letters[i] = "B";
+				else if(grades[i] >= 70) letters[i] = "C";
+				else if(grades[i] >= 60) letters[i] = "D";
+				else letters[i] = "F";
+			} else {
+				if(grades[i] >= 3.4) letters[i] = "A";
+				else if(grades[i] >= 2.8) letters[i] = "B";
+				else if(grades[i] >= 2.2) letters[i] = "C";
+				else if(grades[i] >= 1.6) letters[i] = "D";
+				else letters[i] = "F";
+			}
+		}
 	}
 	public double classgrade(int classnum) {
 		double grade=0;
