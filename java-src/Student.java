425d67743093546f841585a115acab20881e77d9
russell-chai <russell.chai@hotmail.com>
2017-07-27 12:26:56 -0700

    connor ur bad

--- java-src/Student.java
+++ java-src/Student.java
@@ -88,12 +88,15 @@ public class Student {
 	}
 	//calculating students grade by averaging all the learning targets
 	public double classgrade(int classnum) {
+		int sumOfWeights = 0;
 		double grade=0;
 		StudentClass temp = schedule[classnum];
 		for(int i=0;i<temp.targets.size();i++) {
-			grade+=temp.targets.get(i).grades[index[classnum]];
+			grade+=temp.targets.get(i).grades[index[classnum]] * temp.targets.get(i).weight;
+			sumOfWeights += temp.targets.get(i).weight;
+			
 		}
-		grade/=temp.targets.size();
+		grade/=sumOfWeights;
 		grades[classnum] = grade;
 		return grade;
 	}
