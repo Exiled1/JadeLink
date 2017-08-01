5de22bbcf2990c03286c1785a3d7b2f8dd9d6ed1
CShu12 <connor.shu@gmail.com>
2017-08-01 10:49:37 -0700

    Added more dummy data .sql files

---
+++ dummy-data/dentist_info.sql
@@ -0,0 +1,11 @@
+create table dentist_info (
+	dentist_id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone VARCHAR(50)
+);
+insert into dentist_info (dentist_id, first_name, last_name, email, phone) values (1, 'Felike', 'Teape', 'fteape0@wordpress.com', '216-(892)238-8720');
+insert into dentist_info (dentist_id, first_name, last_name, email, phone) values (2, 'Tonnie', 'Heliot', 'theliot1@mozilla.org', '381-(342)239-2681');
+insert into dentist_info (dentist_id, first_name, last_name, email, phone) values (3, 'Kellen', 'Tonn', 'ktonn2@histats.com', '965-(956)779-6800');
+insert into dentist_info (dentist_id, first_name, last_name, email, phone) values (4, 'Rainer', 'Pietranek', 'rpietranek3@issuu.com', '7-(799)906-0757');
---
+++ dummy-data/doctor_info.sql
@@ -0,0 +1,11 @@
+create table doctor_info (
+	doctor_id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	email VARCHAR(50),
+	phone VARCHAR(50)
+);
+insert into doctor_info (doctor_id, first_name, last_name, email, phone) values (1, 'Anneliese', 'Baggaley', 'abaggaley0@blogspot.com', '86-(223)593-7851');
+insert into doctor_info (doctor_id, first_name, last_name, email, phone) values (2, 'Gery', 'Pymar', 'gpymar1@huffingtonpost.com', '63-(378)994-2088');
+insert into doctor_info (doctor_id, first_name, last_name, email, phone) values (3, 'Marinna', 'Soigne', 'msoigne2@nationalgeographic.com', '62-(303)885-0830');
+insert into doctor_info (doctor_id, first_name, last_name, email, phone) values (4, 'Gan', 'Harvey', 'gharvey3@dedecms.com', '506-(937)936-9193');
